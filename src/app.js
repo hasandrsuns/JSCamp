a44f9c40e6d943cf6bcd1c8628454c15310fd470
hasandrsuns <hasandrsuns@gmail.com>
2021-10-26 17:59:56 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.50
+
+console.log(dolarDun )
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 //Bunu yapamıyoruz 
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Özel konut kredisi"]
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+    
+}
